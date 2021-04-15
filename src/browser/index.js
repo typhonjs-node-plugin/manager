f5772823c2f10d3cb4cf65e6dd9f598eea4cd6c3
typhonrt <support@typhonrt.org>
2021-04-15 14:44:43 -0700

    export Eventbus / EventbusProxy as named exports

--- src/browser/index.js
+++ src/browser/index.js
@@ -2,4 +2,6 @@ import BrowserPluginManager         from './BrowserPluginManager.js';
 
 export { default as isValidConfig } from '../isValidConfig.js';
 
+export { default as Eventbus, EventbusProxy } from '@typhonjs-plugin/eventbus';
+
 export default BrowserPluginManager;
