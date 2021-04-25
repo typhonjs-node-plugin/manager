90505d20ed7def4af682ed408819799912ac8b2f
typhonrt <support@typhonrt.org>
2021-04-25 11:11:57 -0700

    update build-test-browser

--- test/src/browser/run.js
+++ test/src/browser/run.js
@@ -9,24 +9,14 @@ fs.copySync('./test/fixture', './test/live-server/test/fixture');
 
 (async () =>
 {
-   // await TestRunner.runServerAndTestSuite({ reportDir: './coverage-browser' });
+   await TestRunner.runServerAndTestSuite({ reportDir: './coverage-browser' });
 
-   // Uncomment to keep live server alive; useful when manually testing Firefox with live server still running.
-   const { passed } = await TestRunner.runServerAndTestSuite({ reportDir: './coverage-browser', keepAlive: true });
-
-   process.stdout.write('Hit `ctrl-c` to exit.')
-
-   const stdin = process.stdin;
-
-   stdin.setRawMode( true );
-   stdin.resume();
-   stdin.setEncoding( 'utf8' );
-
-   stdin.on( 'data', (key) =>
-   {
-      // ctrl-c ( end of text )
-      if (key === '\u0003') { process.exit(passed ? 0 : 1); }
-   });
+   // Uncomment to keep live server alive; useful when manually testing Firefox, etc.
+   // await TestRunner.runServerAndTestSuite({
+   //    reportDir: './coverage-browser',
+   //    keepAlive: true,
+   //    stdinLatch: true
+   // });
 })().catch((err) =>
 {
    console.log(err);
