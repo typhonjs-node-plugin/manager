ab14e9efacf0196201f7b1538fd8036f82b75bdd
typhonrt <support@typhonrt.org>
2021-06-03 01:39:08 -0700

    update testsuite

--- rollup.config.browser-test.js
+++ rollup.config.browser-test.js
@@ -4,8 +4,8 @@ import { babel }  from '@rollup/plugin-babel';        // Babel is used for priva
 import istanbul   from 'rollup-plugin-istanbul';      // Adds Istanbul instrumentation.
 import resolve    from '@rollup/plugin-node-resolve'; // This resolves NPM modules from node_modules.
 
-// The test browser distribution is bundled to `./test/live-server`.
-const s_TEST_BROWSER_PATH = './test/live-server';
+// The test browser distribution is bundled to `./test/public`.
+const s_TEST_BROWSER_PATH = './test/public';
 
 // Produce sourcemaps or not.
 const s_SOURCEMAP = true;
@@ -41,12 +41,15 @@ export default () =>
 
       // This bundle is the test suite
       {
-         input: ['test/src/runner/TestSuiteRunner.js'],
+         input: ['test/src/runner/TestsuiteRunner.js'],
          output: [{
-            file: `${s_TEST_BROWSER_PATH}/TestSuiteRunner.js`,
+            file: `${s_TEST_BROWSER_PATH}/TestsuiteRunner.js`,
             format: 'es',
             preferConst: true
-         }]
+         }],
+         plugins: [
+            resolve({ browser: true })
+         ]
       }
    ];
 };
