d941578e1a007cefc9a229fb18e1dd82228c2bcd
typhonrt <support@typhonjs.io>
2023-05-06 15:00:52 -0700

    Comments

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,12 +6,13 @@ import { importsExternal } from "@typhonjs-build-test/rollup-external-imports";
 const sourcemap = true;
 
 // Bundle all top level external package exports.
-const dtsPluginOptions = { bundlePackageExports: true };
+const dtsEventbusOptions = { bundlePackageExports: true, prependFiles: ['./module.desc'] };
+const dtsManagerOptions = { prependFiles: ['./module.desc'] };
 
 export default () =>
 {
    return [
-      {   // This bundle is for the Node distribution.
+      {   // This bundle is for the main eventbus subpath export
          input: './src/eventbus/index.js',
          output: [{
             file: `./dist/eventbus/index.js`,
@@ -22,11 +23,26 @@ export default () =>
          plugins: [
             importsExternal(),
             resolve(),
-            generateDTS.plugin(dtsPluginOptions)
+            generateDTS.plugin(dtsEventbusOptions)
          ]
       },
 
-      {   // This bundle is for the Node distribution.
+      {   // This bundle is for the pre-defined eventbus buses subpath export
+         input: './src/eventbus/buses/index.js',
+         output: [{
+            file: `./dist/eventbus/buses/index.js`,
+            format: 'es',
+            generatedCode: { constBindings: true },
+            sourcemap
+         }],
+         plugins: [
+            importsExternal(),
+            resolve(),
+            generateDTS.plugin(dtsEventbusOptions)
+         ]
+      },
+
+      {   // This bundle is for the plugin manager Node distribution.
          input: './src/manager/index.js',
          output: [{
             file: `./dist/manager/node/index.js`,
@@ -37,11 +53,11 @@ export default () =>
          plugins: [
             importsExternal(),
             resolve({ exportConditions: ['node', 'import'] }),
-            generateDTS.plugin()
+            generateDTS.plugin(dtsManagerOptions)
          ]
       },
 
-      // This bundle is for the browser distribution.
+      // This bundle is for the plugin manager browser distribution.
       {
          input: './src/manager/index.js',
          output: [{
@@ -53,7 +69,7 @@ export default () =>
          plugins: [
             importsExternal(),
             resolve({ exportConditions: ['browser', 'import'] }),
-            generateDTS.plugin()
+            generateDTS.plugin(dtsManagerOptions)
          ]
       }
    ];
